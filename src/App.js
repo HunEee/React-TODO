b8a6a7cf9783051abcd34cd43e24449f7d43666b
HunEee <asdf981001@naver.com>
2025-05-07 22:33:33 +0900

    로그인페이지

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
 import './App.css';
-import Counter from './components/counter/Counter';
+import TodoApp from './components/todo/TodoApp'
 
 
 function App() {
   return (
     <div className="App">
-      <Counter/>
+      <TodoApp />
     </div>
   );
 }
 
-// 프로퍼티 전달 방법 1
-// {property1: 'value1', property2: 'value2'}
-// function PlayingWithProps(properties){
-//   console.log(properties)
-//   console.log(properties.property1)
-//   console.log(properties.property2)
-//   return(
-//     <div>Props</div>
-//   )
-// }
-
-// 프로퍼티 전달 방법 2
-// function PlayingWithProps({property1,property2}){
-//   console.log(property1)
-//   console.log(property2)
-//   return(
-//     <div>Props</div>
-//   )
-// }
 
 
 export default App;
