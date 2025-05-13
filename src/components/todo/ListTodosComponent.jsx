e332fbd86bc127511fc13002699ccbd43e456bf9
HunEee <asdf981001@naver.com>
2025-05-13 23:28:54 +0900

    Todo 리스트 Rest API 만들기

--- src/components/todo/ListTodosComponent.jsx
+++ src/components/todo/ListTodosComponent.jsx
@@ -1,26 +1,59 @@
+import {useEffect, useState } from "react"
+import {retrieveAllTodosForUsernameApi, deleteTodoApi} from "./api/TodoApiService"
+
 export default function ListTodosComponent(){
 
     const today = new Date()
     const targetDate = new Date(today.getFullYear()+12, today.getMonth(), today.getDay())
 
-    const todos = [
-                        {id: 1, description: 'Learn AWS', done: false, targetDate:targetDate},
-                        {id: 2, description: 'Learn Full Stack Dev', done: false, targetDate:targetDate},
-                        {id: 3, description: 'Learn DevOps', done: false, targetDate:targetDate}
-                ]
+    // 리스트를 가져오기 위한 state
+    const[todos,setTodos] = useState([])
+    // 삭제 알림을 위한 state
+    const[message,setMessage] = useState(null)
 
+    // const todos = [
+    //                     // {id: 1, description: 'Learn AWS', done: false, targetDate:targetDate},
+    //                     // {id: 2, description: 'Learn Full Stack Dev', done: false, targetDate:targetDate},
+    //                     // {id: 3, description: 'Learn DevOps', done: false, targetDate:targetDate}
+    //             ]
+
+    // ,[] 을 붙여야 로드할때 렌더링함 
+    useEffect(() => refreshTodos(),[])
+
+    // 전체 Todo 리스트 API
+    function refreshTodos(){
+        retrieveAllTodosForUsernameApi('in28minutes')
+            .then(response => {
+                console.log(response)
+                setTodos(response.data)   
+            })
+            .catch(error=>console.log(error))
+    }
+
+    // delete API
+    function deleteTodo(id){
+        console.log('clicked'+id)
+        deleteTodoApi('in28minutes',id)
+            .then(
+                () => {
+                    setMessage(`Delete of todo with id = ${id} successful`)
+                    refreshTodos()
+            })
+            .catch(error => console.log(error))
+    }
 
     return(
         <div className="container">
             <h1>Things You Want To Do!</h1>
+            {message && <div className="alert alert-warning">{message}</div>}
             <div>
                <table className='table'>
                     <thead>
                         <tr>
-                            <td>id</td>
-                            <td>description</td>
-                            <td>Is done?</td>
-                            <td>targetDate</td>
+                            <th>Description</th>
+                            <th>Is done?</th>
+                            <th>TargetDate</th>
+                            <th>Delete</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -28,10 +61,11 @@ export default function ListTodosComponent(){
                             todos.map(
                                 todo => (
                                     <tr key={todo.id}>
-                                        <td>{todo.id}</td>
                                         <td>{todo.description}</td>
                                         <td>{todo.done.toString()}</td>
-                                        <td>{todo.targetDate.toDateString()}</td>
+                                        {/* <td>{todo.targetDate.toDateString()}</td> */}
+                                        <td>{todo.targetDate.toString()}</td>
+                                        <td><button className="btn btn-warning" onClick={()=>deleteTodo(todo.id)}>Delete</button></td>
                                      </tr>
                                 )
                             )
